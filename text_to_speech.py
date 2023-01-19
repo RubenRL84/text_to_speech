7f840b85d53116c2800d5c7009a2a364be8ee903
Rúben Lacerda <51936170+RubenRL84@users.noreply.github.com>
2023-01-19 00:45:09 +0000

    update to pt-pt language

--- text_to_speech.py
+++ text_to_speech.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     # here we have marked slow=False. Which tells 
     # the module that the converted audio should 
     # have a high speed
-    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj = gTTS(text=mytext, tld=language, slow=False)
 
     # Saving the converted audio in a mp3 file named
     # welcome 
